c15f2d2d0948722cb56915b2d90acba0464196a6
Ashquinn Gordon <ashquinngordon@gmail.com>
2021-06-19 17:45:42 -0400

    Product and Tags association added to model

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -16,8 +16,26 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'tagged_product'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'product_tags'
+});
 
 module.exports = {
   Product,
