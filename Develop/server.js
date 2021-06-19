57dc338ba3b21f65f1736108ae76b1e8bf7134b7
Ashquinn Gordon <ashquinngordon@gmail.com>
2021-06-19 18:08:52 -0400

    server.js file reformatted (white space deleted)

--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
 const mysql = require('mysql2');
-// import sequelize connection
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,7 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
