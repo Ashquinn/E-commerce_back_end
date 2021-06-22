1425c4b0ec95f6be70bd1dc568a70e71851df631
Ashquinn Gordon <ashquinngordon@gmail.com>
2021-06-22 13:51:17 -0400

    PUT and POST routes added for tag-routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -34,11 +34,28 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    const tagData = await Tag.create(req.body)
+    res.status(200).json(tagData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.update(req.body,
+      {
+        where: {
+          id: req.params.id
+        }
+      })
+      res.status(200).json(tagData)
+  } catch (err) {
+    res.status(500).json(err)
+  }
   // update a tag's name by its `id` value
 });
 
